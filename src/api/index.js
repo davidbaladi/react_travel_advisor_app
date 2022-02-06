b0d8aa13867f0a30b2601e9bf9862bb3aab63490
davidbaladi <davidbaladi@gmail.com>
2022-02-05 21:00:12 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat: lat, lon: lng },
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GoogleMapsApp from "./GoogleMapsApp";
+
+ReactDOM.render(<GoogleMapsApp />, document.getElementById("root"));
